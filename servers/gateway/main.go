0b0975d6b4ab83f6f21f9c891b518ba62287eaf4
evanfrawley <frawley@uw.edu>
2017-10-21 03:11:22 -0700

    Add session and user complete

--- servers/gateway/main.go
+++ servers/gateway/main.go
@@ -6,6 +6,9 @@ import (
     "fmt"
     "log"
     "github.com/info344-a17/challenges-evanfrawley/servers/gateway/handlers"
+    "github.com/info344-a17/challenges-evanfrawley/servers/gateway/sessions"
+    //"github.com/go-redis/redis"
+    //"time"
 )
 
 //main is the main entry point for the server
@@ -15,11 +18,28 @@ func main() {
         localAddr = ":443"
     }
 
-    tlsKeyPath := os.Getenv("TLSKEY")
-    tlsCertPath := os.Getenv("TLSCERT")
+    //tlsKeyPath := os.Getenv("TLSKEY")
+    //tlsCertPath := os.Getenv("TLSCERT")
+
+    tlsKeyPath := "/Users/evanfrawley/go/src/github.com/info344-a17/challenges-evanfrawley/servers/gateway/tls/privkey.pem"
+    tlsCertPath := "/Users/evanfrawley/go/src/github.com/info344-a17/challenges-evanfrawley/servers/gateway/tls/fullchain.pem"
 
     fmt.Printf("Go port: %s \n", localAddr)
     mux := http.NewServeMux()
+    sessionID, _ := sessions.NewSessionID("nice")
+    sessions.ValidateID(string(sessionID), "nice")
+
+    //client := redis.NewClient(&redis.Options{
+    //    Addr:     "localhost:6379",
+    //    Password: "", // no password set
+    //    DB:       0,  // use default DB
+    //})
+    //store := sessions.NewRedisStore(client, time.Hour)
+    //
+    //pong, err := client.Ping().Result()
+    //fmt.Println(pong, err)
+
+    //sessions.BeginSession("nice", store, )
 
     mux.HandleFunc("/v1/summary", handlers.SummaryHandler)
 
--- servers/gateway/sessions/redisstore.go
+++ servers/gateway/sessions/redisstore.go
@@ -4,6 +4,9 @@ import (
 	"time"
 
 	"github.com/go-redis/redis"
+    "fmt"
+    "encoding/json"
+    "reflect"
 )
 
 //RedisStore represents a session.Store backed by redis.
@@ -17,7 +20,10 @@ type RedisStore struct {
 //NewRedisStore constructs a new RedisStore
 func NewRedisStore(client *redis.Client, sessionDuration time.Duration) *RedisStore {
 	//initialize and return a new RedisStore struct
-	return nil
+	return &RedisStore {
+		Client: client,
+		SessionDuration: sessionDuration,
+	}
 }
 
 //Store implementation
@@ -26,30 +32,46 @@ func NewRedisStore(client *redis.Client, sessionDuration time.Duration) *RedisSt
 //The `sessionState` parameter is typically a pointer to a struct containing
 //all the data you want to associated with the given SessionID.
 func (rs *RedisStore) Save(sid SessionID, sessionState interface{}) error {
-	//TODO: marshal the `sessionState` to JSON and save it in the redis database,
-	//using `sid.getRedisKey()` for the key.
-	//return any errors that occur along the way.
+    sessionStateBytes, err := json.Marshal(sessionState)
+    if err != nil {
+        return fmt.Errorf("error when marshaling JSON: %v", err)
+    }
+
+    sessionStateJSONString := string(sessionStateBytes)
+
+	_, err = rs.Client.Set(sid.getRedisKey(), sessionStateJSONString, rs.SessionDuration).Result()
+	if err != nil {
+        return fmt.Errorf("error saving to redis store: %v", err)
+    }
 	return nil
 }
 
 //Get populates `sessionState` with the data previously saved
 //for the given SessionID
 func (rs *RedisStore) Get(sid SessionID, sessionState interface{}) error {
-	//TODO: get the previously-saved session state data from redis,
-	//unmarshal it back into the `sessionState` parameter
-	//and reset the expiry time, so that it doesn't get deleted until
-	//the SessionDuration has elapsed.
+    pipe := rs.Client.Pipeline()
 
-	//for extra-credit using the Pipeline feature of the redis
-	//package to do both the get and the reset of the expiry time
-	//in just one network round trip!
+	getCmd := pipe.Get(sid.getRedisKey())
+    pipe.Expire(sid.getRedisKey(), rs.SessionDuration)
+
+    _, err := pipe.Exec()
+    if err != nil {
+        return ErrStateNotFound
+    }
+
+    // unmarshal
+    sessionStateUnmarshal := json.Unmarshal([]byte(getCmd.Val()), sessionState)
+    sessionState = reflect.ValueOf(sessionStateUnmarshal)
 
 	return nil
 }
 
 //Delete deletes all state data associated with the SessionID from the store.
 func (rs *RedisStore) Delete(sid SessionID) error {
-	//TODO: delete the data stored in redis for the provided SessionID
+	_, err := rs.Client.Del(sid.getRedisKey()).Result()
+	if err != nil {
+	    return fmt.Errorf("error removing key '%s' from redis store: %v", sid, err)
+    }
 	return nil
 }
 
